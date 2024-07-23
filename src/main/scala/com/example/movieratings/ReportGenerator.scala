ba0ef26acaf65f8810a0989c23f1976097bb6312
alduk <arsenkudla@compstak.com>
2024-07-23 14:32:24 +0300

    Initial commit

---
+++ src/main/scala/com/example/movieratings/ReportGenerator.scala
@@ -0,0 +1,9 @@
+package com.example.movieratings
+
+object ReportGenerator {
+
+  def main(args: Array[String]): Unit = {
+    // check out CsvUtils for methods to read and write CSV files
+  }
+
+}
